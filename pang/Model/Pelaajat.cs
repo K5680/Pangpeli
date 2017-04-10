2c627815a04f6303e78a3c52b3af13178eba4333
k5680 <k5680@student.jamk.fi>
2017-04-10 15:35:45 +0300

    v4.1 - viewmodel kesken

---
+++ pang/Model/Pelaajat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pang.Model
+{
+         /// <summary>
+        /// This class holds Pelaajat properties.
+        /// </summary>
+        public class Pelaajat
+        {            
+            public string PlayerName { get; set; }
+            public int PlayerPoints { get; set; }
+            
+
+            public string FullInfo
+            {
+                get
+                {
+                return PlayerName + " " + PlayerPoints;
+                }
+            }
+        }
+    }
